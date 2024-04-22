4704f3c2dd436e4a5215f1c0c27fe106af2a33f9
pablo.quadros <pablo.quadros@allog.com.br>
2024-04-22 09:38:14 -0300

    feat: Add Notifications

---
+++ src/Project.Business/Interfaces/INotifier.cs
@@ -0,0 +1,10 @@
+﻿using Project.Business.Notifications;
+
+namespace Project.Business.Interfaces;
+
+public interface INotifier
+{
+    bool HasNotification();
+    List<Notification> GetNotificacoes();
+    void Handle(Notification notification);
+}
---
+++ src/Project.Business/Notifications/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace Project.Business.Notifications;
+
+public class Notification
+{
+    public Notification(string message)
+    {
+        Message = message;
+    }
+
+    public string Message { get; }
+}
